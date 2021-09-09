8e0552f8dd81dbc423eefe4423b6cf9157bf3ec0
CynthiaAdhiambo <larkmacreen@gmail.com>
2021-09-09 20:48:35 +0300

    added some heading to the attractions and detination pages

--- src/app/components/destinations/destinations.component.ts
+++ src/app/components/destinations/destinations.component.ts
@@ -1,41 +1,36 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
 import { DataService } from 'src/app/data.service';
 import { toursparams } from 'src/tour';
-
 
 @Component({
   selector: 'app-destinations',
   templateUrl: './destinations.component.html',
-  styleUrls: ['./destinations.component.css']
+  styleUrls: ['./destinations.component.css'],
 })
-// 
+//
 export class DestinationsComponent implements OnInit {
   //
   //define the destinations variable
-  destinations: toursparams[]=[];
+  destinations: toursparams[] = [];
   isLoading: boolean = true;
   //
   //create a new instance of dataservice from the imported data.service
-  constructor(private dataService: DataService, private router: Router, private breakpointObserver: BreakpointObserver) {
-      
-   }
-   //
+  constructor(private dataService: DataService, private router: Router) {}
+  //
   ngOnInit() {
     //
     //Call the getdestinations method of the service
     this.getDestinations();
   }
-  getDestinations(): void{
+  getDestinations(): void {
     this.isLoading = true;
     //
-    this.dataService.getdestinations().subscribe((data: toursparams[])=>{
-      // 
-      this.destinations=(data);
+    this.dataService.getdestinations().subscribe((data: toursparams[]) => {
+      //
+      this.destinations = data;
       this.isLoading = false;
+      console.log('Destinations: ', this.destinations);
     });
   }
   //
@@ -43,10 +38,4 @@ export class DestinationsComponent implements OnInit {
   getAtrractions(destination: string) {
     this.router.navigate(['/attraction', destination]);
   }
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-  .pipe(
-    map(result => result.matches),
-    shareReplay()
-  );
 }
