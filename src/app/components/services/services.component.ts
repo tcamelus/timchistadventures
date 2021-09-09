e3e143a7e86a76cd5f6c411a4ab57096ccff76b5
CynthiaAdhiambo <larkmacreen@gmail.com>
2021-09-09 22:20:32 +0300

    added loader to the services component

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -6,7 +6,7 @@ import { toursparams } from 'src/tour';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent implements OnInit {
   //
@@ -20,27 +20,26 @@ export class ServicesComponent implements OnInit {
   //
   type!: string;
   //
-  data: any[]=[];
+  data: any[] = [];
   //define the attraction array
-  services: toursparams[]=[];
+  services: toursparams[] = [];
+  isLoading: boolean = true;
   //
   //define the actual service in an attraction
-  atservices: toursparams[]=[];
-  
-  
+  atservices: toursparams[] = [];
+
   constructor(
     private dataService: DataService,
     private activeRoute: ActivatedRoute,
     private plans: PlanningService
-  ) 
-  {
+  ) {
     this.activeRoute.params.subscribe((p) => {
       this.selectedAttraction = p['attraction'];
       this.getServices(this.selectedAttraction);
-      this.serviceID =p['service'];
-      this.type =p['type'];
+      this.serviceID = p['service'];
+      this.type = p['type'];
       this.getServiceDetails(this.serviceID, this.type);
-    }); 
+    });
   }
 
   ngOnInit(): void {
@@ -49,29 +48,32 @@ export class ServicesComponent implements OnInit {
   }
   //
   //get the services from incoming data
-  getServices(selectedAttraction: string){
-    let attraction = this.selectedAttraction
-    this.dataService.getServices(attraction).
-    subscribe((data: toursparams [])=>{
-      // 
-      this.services=(data);
-      //console.log("Services: ", data);
-    });
+  getServices(selectedAttraction: string) {
+    this.isLoading = true;
+    let attraction = this.selectedAttraction;
+    this.dataService
+      .getServices(attraction)
+      .subscribe((data: toursparams[]) => {
+        //
+        this.services = data;
+        this.isLoading = false;
+      });
   }
-  getServiceDetails($serviceID: string, $type: string){
-    // 
-    this.dataService.getServiceDetails(this.selectedAttraction, $serviceID, $type).
-    subscribe((data: toursparams [])=>{
-      // 
-      this.atservices=(data);
-      //console.log("Services: ", data);
-    });
+  getServiceDetails($serviceID: string, $type: string) {
+    //
+
+    this.dataService
+      .getServiceDetails(this.selectedAttraction, $serviceID, $type)
+      .subscribe((data: toursparams[]) => {
+        //
+        this.atservices = data;
+      });
   }
   //
   //Add services to the plans
-  addService(atservices: toursparams){
+  addService(atservices: toursparams) {
     //get the attraction and service
-    this.plans.addService(atservices)
-    window.alert("service added to plans!");
+    this.plans.addService(atservices);
+    window.alert('service added to plans!');
   }
 }
